ed58c541e0417f69dc626dc9c1da997d1273b381
Vyacheslav <viacheslav87klerik@gmail.com>
2022-10-05 00:12:18 +0300

    Переделал.

--- src/Books.java
+++ src/Books.java
@@ -1,11 +1,15 @@
 public class Books {
-    // String authorName;
+    private Authors authorName;
+    private Authors authorSurname;
+
     private int publisherAge;
     private String title;
 
-    public Books(String title, int publisherAge) {
+    public Books(String title, int publisherAge, Authors authorName, Authors authorSurname) {
         this.title = title;
         this.publisherAge = publisherAge;
+        this.authorName = authorName;
+        this.authorSurname = authorSurname;
     }
     public String getTitle() {
         return this.title;
@@ -18,4 +22,13 @@ public class Books {
     public void setPublisherAge(int publisherAge) {
         this.publisherAge = publisherAge;
     }
+
+    private Authors getAuthorsName() {
+        return this.authorName;
+    }
+
+    private Authors getAuthorsSurname() {
+        return this.authorSurname;
+    }
+
 }
